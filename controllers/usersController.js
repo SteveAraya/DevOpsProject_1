b30bb128435b3e207c6e6165748147bd2aba1a7a
Steve Araya Solórzano <stevenaraya.24@gmail.com>
2021-02-27 14:01:41 -0600

    User endpoint finished

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,10 +12,18 @@ const user = express();
 // ===================== GET USERS BY PLANT MANAGER =====================
 user.get('/', (req, res, next) => {
 
+    const user = {
+        'user': 'saraya', 
+        'name': 'Steve Araya Solórzano',
+        'cellphone': '85254117',
+        'email': 'stevenaraya.24@gmail.com',
+        'address': 'Alajuela/San Carlos/La Tigra'
+    }
+
     res.status(200).json({
         ok: true,
         message: 'Get de usuario éxitoso.',
-        user: {'user': 'saraya', 'name': 'Steve Araya Solórzano'}
+        user: user
     });
 
 });
